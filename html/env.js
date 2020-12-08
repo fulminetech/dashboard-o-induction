97fcf466ab97805e136adab5ef93f71fccf3f211
marutimuthu <marutimuthu@gmail.com>
2020-12-08 23:52:58 +0530

    Production Test Ongoing

--- html/env.js
+++ html/env.js
@@ -2,6 +2,6 @@
 var hostname = "http://192.168.1.102"
 
 // APIs
-var machineURL = `${hostname}:5000/api/machine`;
+var machineURL = `${hostname}:3128/api/machine`;
 
 var seturl = `${hostname}:3128/set`
--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #http tunnel port 3000
-/home/pi/dashboard-o/ngrok http 3000
+/home/pi/dashboard-o/ngrok http 8080
 #TCP tunnel port 22
 #/home/pi/dashboard-o/ngrok tcp 22
