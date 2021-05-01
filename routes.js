a39a4a64e5ecba1dcd3673d9ecd3fe748b64b575
marutimuthu <marutimuthu@gmail.com>
2021-05-01 14:12:42 +0530

    Committing to main

--- routes.js
+++ routes.js
@@ -38,6 +38,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/html/index.html"));
 });
 
+app.get("/control", (req, res) => {
+    res.sendFile(path.join(__dirname + "/html/control.html"));
+});
+
 app.get("/restart/:what", (req, res) => {
 
     const a = req.params.what;
